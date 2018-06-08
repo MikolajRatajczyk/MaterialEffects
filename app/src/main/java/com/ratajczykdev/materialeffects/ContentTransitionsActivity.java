ec4e61ede3ec4e35a07d3748a1704518738bb09b
MikolajRatajczyk <37159921+MikolajRatajczyk@users.noreply.github.com>
2018-06-08 22:49:00 +0200

    Add Slide enter transition for ContentTransitionsActivity

--- app/src/main/java/com/ratajczykdev/materialeffects/ContentTransitionsActivity.java
+++ app/src/main/java/com/ratajczykdev/materialeffects/ContentTransitionsActivity.java
@@ -1,7 +1,10 @@
 package com.ratajczykdev.materialeffects;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.transition.Slide;
+import android.view.Gravity;
+import android.view.animation.AnimationUtils;
 
 public class ContentTransitionsActivity extends AppCompatActivity
 {
@@ -11,5 +14,13 @@ public class ContentTransitionsActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content_transitions);
+
+        //  create whole animation from code (not XML)
+        Slide slide = new Slide(Gravity.BOTTOM);
+        slide.addTarget(R.id.content_transitions_activity_title_textview);
+        slide.addTarget(R.id.content_transitions_activity_description_textview);
+        slide.setInterpolator(AnimationUtils.loadInterpolator(this, android.R.interpolator.linear_out_slow_in));
+        slide.setDuration(300);
+        getWindow().setEnterTransition(slide);
     }
 }
