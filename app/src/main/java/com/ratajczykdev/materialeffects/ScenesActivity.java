8bcf20ee2f8e9259201c7de4ba40917ab60f568a
MikolajRatajczyk <37159921+MikolajRatajczyk@users.noreply.github.com>
2018-06-08 14:04:48 +0200

    Add standard transition for ScenesActivity

--- app/src/main/java/com/ratajczykdev/materialeffects/ScenesActivity.java
+++ app/src/main/java/com/ratajczykdev/materialeffects/ScenesActivity.java
@@ -1,15 +1,35 @@
 package com.ratajczykdev.materialeffects;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.transition.Scene;
+import android.transition.TransitionManager;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 public class ScenesActivity extends AppCompatActivity
 {
+    private ImageButton imageButtonInfo;
+    private LinearLayout linearLayoutRoot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scenes);
+
+        linearLayoutRoot = findViewById(R.id.scenes_activity_root);
+
+        imageButtonInfo = findViewById(R.id.scenes_activity_fox_info_imagebutton);
+        imageButtonInfo.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                TransitionManager.go(Scene.getSceneForLayout(linearLayoutRoot, R.layout.activity_scenes_detailed, ScenesActivity.this));
+            }
+        });
+
     }
 }
