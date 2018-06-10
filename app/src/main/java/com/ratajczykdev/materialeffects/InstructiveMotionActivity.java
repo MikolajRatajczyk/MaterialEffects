23b1176a8b3d9bd18f3e9f3416cb6468642d73a5
MikolajRatajczyk <37159921+MikolajRatajczyk@users.noreply.github.com>
2018-06-10 15:56:20 +0200

    Add animation for scrollView

--- app/src/main/java/com/ratajczykdev/materialeffects/InstructiveMotionActivity.java
+++ app/src/main/java/com/ratajczykdev/materialeffects/InstructiveMotionActivity.java
@@ -1,15 +1,32 @@
 package com.ratajczykdev.materialeffects;
 
-import android.support.v7.app.AppCompatActivity;
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ScrollView;
 
 public class InstructiveMotionActivity extends AppCompatActivity
 {
+    private ScrollView scrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instructive_motion);
+
+        scrollView = findViewById(R.id.instructive_motion_activity_scroll_view);
+    }
+
+    @Override
+    public void onEnterAnimationComplete()
+    {
+        //  when window transition ends
+        super.onEnterAnimationComplete();
+        final int startScrollPosition = getResources().getDimensionPixelSize(R.dimen.init_scroll_up_distance);
+        //  animate scrollY property of the scrollView
+        Animator animator = ObjectAnimator.ofInt(scrollView, "scrollY", startScrollPosition).setDuration(300);
+        animator.start();
     }
 }
