225fe779e1d1dbb51f660a53eece65c27bc5ae2c
MikolajRatajczyk <37159921+MikolajRatajczyk@users.noreply.github.com>
2018-06-10 18:20:57 +0200

    Add spinnerInterpolators to InterpolationActivity

--- app/src/main/java/com/ratajczykdev/materialeffects/InterpolationActivity.java
+++ app/src/main/java/com/ratajczykdev/materialeffects/InterpolationActivity.java
@@ -1,15 +1,24 @@
 package com.ratajczykdev.materialeffects;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class InterpolationActivity extends AppCompatActivity
 {
+    private Spinner spinnerInterpolators;
+    private Spinner spinnerDurations;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interpolation);
+
+        ArrayAdapter<CharSequence> arrayAdapterForInterpolatorsSpinner = ArrayAdapter.createFromResource(this, R.array.interpolators_array, android.R.layout.simple_spinner_item);
+        arrayAdapterForInterpolatorsSpinner.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerInterpolators = findViewById(R.id.interpolation_activity_interpolators_spinner);
+        spinnerInterpolators.setAdapter(arrayAdapterForInterpolatorsSpinner);
     }
 }
