5d5ce9680c8007d668ee7a5868fee30b8400afcb
MikolajRatajczyk <37159921+MikolajRatajczyk@users.noreply.github.com>
2018-06-12 00:45:43 +0200

    Add items to CurvedMotionActivity UI

--- app/src/main/java/com/ratajczykdev/materialeffects/coordinatedmotion/CurvedMotionListActivity.java
+++ app/src/main/java/com/ratajczykdev/materialeffects/coordinatedmotion/CurvedMotionListActivity.java
@@ -2,16 +2,29 @@ package com.ratajczykdev.materialeffects.coordinatedmotion;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import com.ratajczykdev.materialeffects.R;
 
 public class CurvedMotionListActivity extends AppCompatActivity
 {
+    private FrameLayout frameLayoutItemRoot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_curved_motion_list);
+
+        frameLayoutItemRoot = findViewById(R.id.curved_motion_activity_item_root);
+        frameLayoutItemRoot.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                //  TODO: start detail activity
+            }
+        });
     }
 }
