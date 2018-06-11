1a333437e939fa801ecaff9bff9fed2ac779859e
MikolajRatajczyk <37159921+MikolajRatajczyk@users.noreply.github.com>
2018-06-12 01:27:35 +0200

    Add shared element transition for avatar

--- app/src/main/java/com/ratajczykdev/materialeffects/coordinatedmotion/CurvedMotionListActivity.java
+++ app/src/main/java/com/ratajczykdev/materialeffects/coordinatedmotion/CurvedMotionListActivity.java
@@ -1,5 +1,6 @@
 package com.ratajczykdev.materialeffects.coordinatedmotion;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +12,7 @@ import com.ratajczykdev.materialeffects.R;
 public class CurvedMotionListActivity extends AppCompatActivity
 {
     private FrameLayout frameLayoutItemRoot;
+    private View viewAvatar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -18,14 +20,18 @@ public class CurvedMotionListActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_curved_motion_list);
 
-        frameLayoutItemRoot = findViewById(R.id.curved_motion_activity_item_root);
+        viewAvatar = findViewById(R.id.curved_motion_list_activity_avatar_view);
+
+        frameLayoutItemRoot = findViewById(R.id.curved_motion_list_activity_item_root);
         frameLayoutItemRoot.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view)
             {
                 Intent intent = new Intent(CurvedMotionListActivity.this, CurvedMotionDetailActivity.class);
-                startActivity(intent);
+                //  use default animation, specify shared element
+                Bundle bundle = ActivityOptions.makeSceneTransitionAnimation(CurvedMotionListActivity.this, viewAvatar, viewAvatar.getTransitionName()).toBundle();
+                startActivity(intent, bundle);
             }
         });
     }
