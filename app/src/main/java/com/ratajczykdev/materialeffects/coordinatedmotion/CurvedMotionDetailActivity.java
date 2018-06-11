b06a72d59a3e35e469ba5df228a018f6f27f0a5c
MikolajRatajczyk <37159921+MikolajRatajczyk@users.noreply.github.com>
2018-06-12 01:38:15 +0200

    Add custom curve transition for avatar

--- app/src/main/java/com/ratajczykdev/materialeffects/coordinatedmotion/CurvedMotionDetailActivity.java
+++ app/src/main/java/com/ratajczykdev/materialeffects/coordinatedmotion/CurvedMotionDetailActivity.java
@@ -1,7 +1,9 @@
 package com.ratajczykdev.materialeffects.coordinatedmotion;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.transition.Transition;
+import android.transition.TransitionInflater;
 
 import com.ratajczykdev.materialeffects.R;
 
@@ -13,5 +15,9 @@ public class CurvedMotionDetailActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_curved_motion_detail);
+
+        Transition customTransition = TransitionInflater.from(this).inflateTransition(R.transition.transition_curve);
+        //  load custom curve transition for shared element
+        getWindow().setSharedElementEnterTransition(customTransition);
     }
 }
