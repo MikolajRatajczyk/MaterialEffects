d9c749aea126c034b864083592bac506078a9e11
MikolajRatajczyk <37159921+MikolajRatajczyk@users.noreply.github.com>
2018-06-11 16:35:02 +0200

    Add Card s animation in MovingCardsActivity UI

--- app/src/main/java/com/ratajczykdev/materialeffects/coordinatedmotion/MovingCardsActivity.java
+++ app/src/main/java/com/ratajczykdev/materialeffects/coordinatedmotion/MovingCardsActivity.java
@@ -1,17 +1,64 @@
 package com.ratajczykdev.materialeffects.coordinatedmotion;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.view.animation.Interpolator;
+import android.widget.LinearLayout;
 
 import com.ratajczykdev.materialeffects.R;
 
 public class MovingCardsActivity extends AppCompatActivity
 {
+    private LinearLayout linearLayoutCardsRoot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_moving_cards);
+
+        linearLayoutCardsRoot = findViewById(R.id.moving_cards_activity_cards_root_view);
+        linearLayoutCardsRoot.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                animateCards();
+            }
+        });
+
+        animateCards();
+    }
+
+    private void animateCards()
+    {
+        final int CARDS_COUNT = linearLayoutCardsRoot.getChildCount();
+        float cardOffsetY = getResources().getDimensionPixelSize(R.dimen.moving_cards_activity_card_offset_y);
+        Interpolator cardInterpolator = AnimationUtils.loadInterpolator(this, android.R.interpolator.linear_out_slow_in);
+
+        final float CARD_START_OPACITY = 0.85f;
+        final float CARD_FINAL_OPACITY = 1f;
+        final float CARD_FINAL_Y_AXIS_VALUE = 0f;
+        final long CARD_ANIMATION_DURATION = 1000L;
+        final float CARD_OFFSET_MULTIPLIER = 1.5f;
+        for (int position = 0; position < CARDS_COUNT; position++)
+        {
+            View card = linearLayoutCardsRoot.getChildAt(position);
+            card.setVisibility(View.VISIBLE);
+            card.setTranslationY(cardOffsetY);
+            //  change card opacity
+            card.setAlpha(CARD_START_OPACITY);
+
+            card.animate()
+                    .translationY(CARD_FINAL_Y_AXIS_VALUE)
+                    .alpha(CARD_FINAL_OPACITY)
+                    .setInterpolator(cardInterpolator)
+                    .setDuration(CARD_ANIMATION_DURATION)
+                    .start();
+
+            cardOffsetY *= CARD_OFFSET_MULTIPLIER;
+        }
     }
 }
