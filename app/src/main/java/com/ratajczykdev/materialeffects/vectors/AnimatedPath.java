47f7e25df01bb1e5d8e9c78d02af64a5622a1157
MikolajRatajczyk <37159921+MikolajRatajczyk@users.noreply.github.com>
2018-06-12 18:01:21 +0200

    Add AnimatedPathActivity and its Button

---
+++ app/src/main/java/com/ratajczykdev/materialeffects/vectors/AnimatedPath.java
@@ -0,0 +1,17 @@
+package com.ratajczykdev.materialeffects.vectors;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ratajczykdev.materialeffects.R;
+
+public class AnimatedPath extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animated_path);
+    }
+}
--- app/src/main/java/com/ratajczykdev/materialeffects/vectors/AnimatedVectorDrawablesMenuActivity.java
+++ app/src/main/java/com/ratajczykdev/materialeffects/vectors/AnimatedVectorDrawablesMenuActivity.java
@@ -1,17 +1,33 @@
 package com.ratajczykdev.materialeffects.vectors;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.ratajczykdev.materialeffects.R;
 
 public class AnimatedVectorDrawablesMenuActivity extends AppCompatActivity
 {
 
+    private Button buttonPath;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animated_vector_drawables_menu);
+
+        buttonPath = findViewById(R.id.animated_vector_drawables_menu_activity_path_button);
+        buttonPath.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AnimatedVectorDrawablesMenuActivity.this, AnimatedPath.class);
+                startActivity(intent);
+            }
+        });
     }
 }
