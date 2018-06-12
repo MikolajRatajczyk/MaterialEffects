03caf8c815254b750ae04c1752ae6073a209b684
MikolajRatajczyk <37159921+MikolajRatajczyk@users.noreply.github.com>
2018-06-12 19:21:02 +0200

    Add animated-vector drawable cross to tick animation

--- app/src/main/java/com/ratajczykdev/materialeffects/vectors/AnimatedPath.java
+++ app/src/main/java/com/ratajczykdev/materialeffects/vectors/AnimatedPath.java
@@ -1,17 +1,42 @@
 package com.ratajczykdev.materialeffects.vectors;
 
-import android.support.v7.app.AppCompatActivity;
+
+import android.graphics.drawable.AnimatedVectorDrawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.ratajczykdev.materialeffects.R;
 
 public class AnimatedPath extends AppCompatActivity
 {
 
+    private ImageView imageViewAnimatedItem;
+    private AnimatedVectorDrawable avdCrossToTick;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animated_path);
+
+        avdCrossToTick = (AnimatedVectorDrawable) getDrawable(R.drawable.avd_cross_to_tick);
+        if (avdCrossToTick != null)
+        {
+            avdCrossToTick.reset();
+        }
+
+        imageViewAnimatedItem = findViewById(R.id.animated_path_activity_animated_item_imageview);
+        imageViewAnimatedItem.setImageDrawable(avdCrossToTick);
+        imageViewAnimatedItem.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                avdCrossToTick.start();
+            }
+        });
+
     }
 }
