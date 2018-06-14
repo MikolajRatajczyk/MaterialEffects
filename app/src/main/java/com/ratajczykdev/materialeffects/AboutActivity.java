e4b17b1565d29582183716d5b9ccee0ea9477e27
MikolajRatajczyk <37159921+MikolajRatajczyk@users.noreply.github.com>
2018-06-14 02:32:18 +0200

    Change AboutActivity UI

--- app/src/main/java/com/ratajczykdev/materialeffects/AboutActivity.java
+++ app/src/main/java/com/ratajczykdev/materialeffects/AboutActivity.java
@@ -1,14 +1,11 @@
 package com.ratajczykdev.materialeffects;
 
 import android.os.Bundle;
-import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.transition.Explode;
-import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 /**
  * @author Mikołaj Ratajczyk <mikolaj.ratajczyk@gmail.com>
@@ -20,7 +17,6 @@ public class AboutActivity extends AppCompatActivity
     private TextView textViewNameSurname;
     private TextView textViewEmail;
     private static final int EXPLODE_TRANSITION_DURATION_IN_MS = 1000;
-    private ConstraintLayout constraintLayoutRoot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -32,7 +28,6 @@ public class AboutActivity extends AppCompatActivity
         textViewCreatedBy = findViewById(R.id.about_activity_created_by_textview);
         textViewNameSurname = findViewById(R.id.about_activity_name_surname_textview);
         textViewEmail = findViewById(R.id.about_activity_email_textview);
-        constraintLayoutRoot = findViewById(R.id.about_activity_root);
 
         //  create whole content transition animation from code (not XML)
         Explode explode = new Explode();
@@ -43,21 +38,5 @@ public class AboutActivity extends AppCompatActivity
         explode.addTarget(textViewNameSurname);
         explode.addTarget(textViewEmail);
         getWindow().setEnterTransition(explode);
-
-        setEasterEgg();
-    }
-
-    private void setEasterEgg()
-    {
-        imageViewStar.setOnLongClickListener(new View.OnLongClickListener()
-        {
-            @Override
-            public boolean onLongClick(View view)
-            {
-                Toast.makeText(AboutActivity.this, "You've discovered easter egg!", Toast.LENGTH_SHORT).show();
-                constraintLayoutRoot.setBackgroundColor(getColor(R.color.colorAccent));
-                return true;
-            }
-        });
     }
 }
